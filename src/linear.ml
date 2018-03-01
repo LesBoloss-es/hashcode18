22e55d1677c7d625cc5ec689de59b7d0a1e56fab
Niols <niols@niols.fr>
2018-03-01 21:11:33 +0100

    check latest finish in linear.one

--- src/linear.ml
+++ src/linear.ml
@@ -2,11 +2,14 @@
 open Common
 
 let one time pos problem ride =
-  (* bonus + duree - temps_total *)
-  (
-    if distance pos ride.start <= time + ride.earliest_start then
-      problem.bonus
-    else
-      0
-  )
-  - distance pos ride.start
+  if time + distance pos ride.start + ride.duration <= ride.latest_finish then
+    (* bonus + duree - temps_total *)
+    (
+      if distance pos ride.start <= time + ride.earliest_start then
+        problem.bonus
+      else
+        0
+    )
+    - distance pos ride.start
+  else
+    min_int
