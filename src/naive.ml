1047efe54f2fedfe59ccce853775828f34894075
Martin Pépin <kerl@wkerl.me>
2018-03-01 20:22:58 +0100

    Fix naive implementation

--- src/naive.ml
+++ src/naive.ml
@@ -3,26 +3,32 @@ open Common
 
 let find_bonus t cur_pos p rides_booked =
   let r = ref (-1) in
+  let end_t = ref t in
   let find i ride =
     if !r = -1 &&
        (not rides_booked.(i)) &&
-       t + distance ride.start cur_pos <= ride.earliest_start then
+       t + distance ride.start cur_pos <= ride.earliest_start then begin
       r := i;
+      end_t := ride.earliest_start + ride.duration
+    end
   in
   Array.iteri find p.rides ;
-  !r
+  !r, !end_t
 
 
 let find_any t cur_pos p rides_booked =
   let r = ref (-1) in
+  let end_t = ref t in
   let find i ride =
     if !r = -1 &&
        (not rides_booked.(i)) &&
-       t + distance ride.start cur_pos + ride.duration <= ride.latest_finish then
+       t + distance ride.start cur_pos + ride.duration <= ride.latest_finish then begin
       r := i;
+      end_t := t + distance ride.start cur_pos + ride.duration
+    end
   in
   Array.iteri find p.rides ;
-  !r
+  !r, !end_t
 
 
 let to_solution cars : solution =
@@ -36,17 +42,17 @@ let i_like_bonuses (p : problem) : solution =
     for i = 0 to p.vehicles - 1 do
       let t, pos, queue, finished = cars.(i) in
       if not finished then begin
-        let r = find_bonus t pos p rides_booked in
-        let r =
-          if r <> -1
-          then r
-          else find_any t pos p rides_booked
+        let r, end_t = find_bonus t pos p rides_booked in
+        let r, end_t =
+          if r = -1
+          then find_any t pos p rides_booked
+          else r, end_t
         in
         if r >= 0 then begin
-          let end_t = t + p.rides.(i).duration in
-          let end_pos = p.rides.(i).finish in
-          let queue = i :: queue in
-          cars.(i) <- (end_t, end_pos, queue, false)
+          rides_booked.(r) <- true;
+          let end_pos = p.rides.(r).finish in
+          let queue = r :: queue in
+          cars.(i) <- (end_t, end_pos, queue, false) ;
         end else begin
           cars.(i) <- t, pos, queue, true
         end
