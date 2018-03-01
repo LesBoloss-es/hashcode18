6f2f65b1abb3266c32f73fabe70b48c156758396
Martin Pépin <kerl@wkerl.me>
2018-03-01 21:56:49 +0100

    meta meta meta

--- src/naive.ml
+++ src/naive.ml
@@ -21,67 +21,8 @@ let find_best (score : time -> intersection -> problem -> ride -> int) t pos p r
   !r
 
 
-let find_bonus t cur_pos p rides_booked =
-  let r = ref (-1) in
-  let end_t = ref t in
-  let find i ride =
-    if !r = -1 &&
-       (not rides_booked.(i)) &&
-       t + distance ride.start cur_pos <= ride.earliest_start then begin
-      r := i;
-      end_t := ride.earliest_start + ride.duration
-    end
-  in
-  Array.iteri find p.rides ;
-  !r, !end_t
-
-
-let find_any t cur_pos p rides_booked =
-  let r = ref (-1) in
-  let end_t = ref t in
-  let find i ride =
-    if !r = -1 &&
-       (not rides_booked.(i)) &&
-       t + distance ride.start cur_pos + ride.duration < ride.latest_finish then begin
-      r := i;
-      end_t := t + distance ride.start cur_pos + ride.duration
-    end
-  in
-  Array.iteri find p.rides ;
-  !r, !end_t
-
-
 let to_solution cars : solution =
   Array.map (fun (_, _, q, _) -> List.rev q) cars
-
-
-(*
-let i_like_bonuses (p : problem) : solution =
-  let cars = Array.make p.vehicles (0, (0, 0), [], false) in
-  let rides_booked = Array.make (Array.length p.rides) false in
-  while Array.exists (fun (_, _, _, finished) -> not finished) cars do
-    for i = 0 to p.vehicles - 1 do
-      let t, pos, queue, finished = cars.(i) in
-      if not finished then begin
-        let r, end_t = find_bonus t pos p rides_booked in
-        let r, end_t =
-          if r = -1
-          then find_any t pos p rides_booked
-          else r, end_t
-        in
-        if r >= 0 then begin
-          rides_booked.(r) <- true;
-          let end_pos = p.rides.(r).finish in
-          let queue = r :: queue in
-          cars.(i) <- (end_t, end_pos, queue, false) ;
-        end else begin
-          cars.(i) <- t, pos, queue, true
-        end
-      end
-    done;
-  done;
-  to_solution cars
-*)
 
 
 let schedule_with_score score (p : problem) : solution =
@@ -105,3 +46,59 @@ let schedule_with_score score (p : problem) : solution =
     done;
   done;
   to_solution cars
+
+
+let naive_choice cars last_chosen =
+  let i = ref last_chosen in
+  let incr () =
+    if !i + 1 = Array.length cars
+    then i := 0
+    else i := !i + 1
+  in
+  let break = ref false in
+  incr () ;
+  while !i <> last_chosen && not !break do
+    let _, _, _, finished = cars.(!i) in
+    if finished
+    then incr ()
+    else break := true
+  done;
+  if !i = last_chosen then -1 else !i
+
+
+let less_advanced cars _ =
+  let i = ref (-1) in
+  let min_t = ref max_int in
+  let select j (t, _, _, finished) =
+    if not finished && t < !min_t then begin
+      i := j;
+      min_t := t
+    end
+  in
+  Array.iteri select cars;
+  !i
+
+
+let schedule_with_score choice score (p : problem) : solution =
+  let cars = Array.make p.vehicles (0, (0, 0), [], false) in
+  let rides_booked = Array.make (Array.length p.rides) false in
+  let i = ref 0 in
+  while !i <> - 1 do
+    let t, pos, queue, finished = cars.(!i) in
+    if not finished then begin
+      let r = find_best score t pos p rides_booked in
+      if r >= 0 then begin
+        rides_booked.(r) <- true;
+        let end_t = end_time t pos p r in
+        let end_pos = p.rides.(r).finish in
+        let queue = r :: queue in
+        cars.(!i) <- (end_t, end_pos, queue, false) ;
+      end else begin
+        cars.(!i) <- t, pos, queue, true
+      end
+    end;
+    i := choice cars !i
+  done;
+  to_solution cars
+
+let schedule_with_score = schedule_with_score naive_choice
